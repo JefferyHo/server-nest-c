2f13495e531a915714d7ae4716621d1f37f1ce52
Jeff <JefferyHo1993@outlook.com>
2023-02-27 20:17:45 +0800

    feat: 添加成功失败的统一返回体

---
+++ src/filters/http-exception/http-exception.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    // const request = ctx.getRequest();
+
+    const message = exception.message;
+
+    let validationMsg: any = exception.getResponse();
+    // console.log(validationMsg, '---')
+    if (typeof validationMsg === 'object') {
+      validationMsg = Array.isArray(validationMsg.message)
+        ? validationMsg.message[0]
+        : validationMsg.messag;
+    }
+    Logger.log('错误提示', validationMsg || message);
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      code: status,
+      message: validationMsg || message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
